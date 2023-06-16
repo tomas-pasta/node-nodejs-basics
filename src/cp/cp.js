8609dd916f87f405ca2acf8bb42622617daa9938
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:26:51 +0200

    add: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,6 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from "child_process";
+
+const spawnChildProcess = async (args) => fork("./src/cp/files/script.js", args);
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
