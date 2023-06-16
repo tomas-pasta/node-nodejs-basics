b0c608b54601a8514341b6b864bef8e8fb339961
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:26:35 +0200

    add: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const argName = args[i].replace(/^--/, "");
+    const argValue = args[i + 1];
+    parsedArgs[argName] = argValue;
+  }
+
+  Object.entries(parsedArgs).forEach(([key, value]) => {
+    console.log(`${key} is ${value}`);
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+const prefix = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = Object.entries(process.env).filter(([key, value]) => key.startsWith(prefix));
+
+  const formattedVariables = rssVariables.map(([key, value]) => `${key}=${value}`).join("; ");
+
+  console.log(formattedVariables);
 };
 
 parseEnv();
