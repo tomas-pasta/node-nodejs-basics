0f543cb3aadd64286fd5e94f46760b989dc648c2
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:27:59 +0200

    add: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import { createHash } from "node:crypto";
+
+const error = "Data operation failed";
+
 const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile('src/hash/files/fileToCalculateHashFor.txt', 'utf8', (err, data) => {
+    if (err) {
+      throw new Error(error)
+    }
+    
+    const hash = createHash('sha256').update(data).digest('hex')
+
+    console.log(hash);
+  })
 };
 
 await calculateHash();
