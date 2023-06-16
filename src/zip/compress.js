ac9d09648fc3675ccf4c28833cd241a7a0007f71
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:28:34 +0200

    add: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createGzip();
+
+    const read = fs.createReadStream('src/zip/files/fileToCompress.txt')
+    
+    const wrire = fs.createWriteStream('src/zip/files/archive.gz')
+
+    read.pipe(gzip).pipe(wrire);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+    const unZip = zlib.createUnzip();
+
+    const read = fs.createReadStream('src/zip/files/archive.gz')
+    
+    const wrire = fs.createWriteStream('src/zip/files/fileToCompress.txt')
+
+    read.pipe(unZip).pipe(wrire);
 };
 
 await decompress();
