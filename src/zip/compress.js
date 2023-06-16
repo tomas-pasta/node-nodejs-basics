d697d63da1d1c8428b99b2c42d6751e751d2530c
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:53:08 +0200

    fix: zip add: file check

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,13 +2,22 @@ import zlib from "zlib";
 import fs from "fs";
 
 const compress = async () => {
-    const gzip = zlib.createGzip();
+  const sourceFilePath = "src/zip/files/fileToCompress.txt";
+  const destinationFilePath = "src/zip/files/archive.gz";
 
-    const read = fs.createReadStream('src/zip/files/fileToCompress.txt')
-    
-    const wrire = fs.createWriteStream('src/zip/files/archive.gz')
+  try {
+    // Проверка наличия исходного файла
+    await fs.promises.access(sourceFilePath, fs.constants.F_OK);
+  } catch (error) {
+    console.log(`Файл ${sourceFilePath} не существует.`);
+    return;
+  }
 
-    read.pipe(gzip).pipe(wrire);
+  const gzip = zlib.createGzip();
+  const read = fs.createReadStream(sourceFilePath);
+  const write = fs.createWriteStream(destinationFilePath);
+
+  read.pipe(gzip).pipe(write);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,13 +2,22 @@ import zlib from "zlib";
 import fs from "fs";
 
 const decompress = async () => {
-    const unZip = zlib.createUnzip();
+  const sourceFilePath = "src/zip/files/archive.gz";
+  const destinationFilePath = "src/zip/files/fileToCompress.txt";
 
-    const read = fs.createReadStream('src/zip/files/archive.gz')
-    
-    const wrire = fs.createWriteStream('src/zip/files/fileToCompress.txt')
+  try {
+    // Проверка наличия исходного файла
+    await fs.promises.access(sourceFilePath, fs.constants.F_OK);
+  } catch (error) {
+    console.log(`Файл ${sourceFilePath} не существует.`);
+    return;
+  }
 
-    read.pipe(unZip).pipe(wrire);
+  const unZip = zlib.createUnzip();
+  const read = fs.createReadStream(sourceFilePath);
+  const wrire = fs.createWriteStream(destinationFilePath);
+
+  read.pipe(unZip).pipe(wrire);
 };
 
 await decompress();
