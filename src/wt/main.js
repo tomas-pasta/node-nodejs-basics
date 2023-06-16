fde070b39726b2f3bdc5c96c7cecbbea80b1f9b9
tomas-pasta <tscik228@gmail.com>
2023-06-16 16:28:26 +0200

    add: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
+const numThreads = cpus().length;
+
+const createWorkerThread = (workerData) => {
+  return new Promise((resolve) => {
+    const worker = new Worker("./src/wt/worker.js", { workerData });
+
+    worker.on("message", (data) => resolve({ status: "resolved", data }));
+
+    worker.on("error", () => resolve({ status: "error", data: null }));
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const promises = [];
+
+  for (let i = 0; i < numThreads; i++) {
+    const workerData = 10 + i;
+    const promise = createWorkerThread(workerData);
+    promises.push(promise);
+  }
+
+  const results = await Promise.all(promises);
+
+  console.log(results);
 };
 
 await performCalculations();
